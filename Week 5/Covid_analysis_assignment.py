eda9e2794bc9eb24547771f3f64447c4dce23adc
Cambros AI <cambrosai@outlook.com>
2025-10-05 04:14:55 +0800

    practise works - Oct 4

--- Week 5/Covid_analysis_assignment.py
+++ Week 5/Covid_analysis_assignment.py
@@ -2,42 +2,41 @@ from utils.data_util import data_util
 from utils.pandas_util import pandas_util
 from utils.visualization_util import visualization_util
 
-class covid_analysis_chart(data_util, pandas_util,visualization_util):
+class covid_analysis_chart(data_util, pandas_util, visualization_util):
 
     def __init__(self, filename):
         self.df = self.read_a_csv(filename)
 
     def plot_top10_confirmed_bar(self):
         df1 = self.df[['Country/Region','Confirmed']].nlargest(10, 'Confirmed')
-        visualization_util().plot_chart(kind='bar', x=df1['Country/Region'], y=df1['Confirmed'], title='Top 10 Countries by Confirmed Cases', xlabel='Country/Region', ylabel='Number of Confirmed Cases', rotation=45, color='orange', edgecolor='black')
+        self.plot_chart(chart_type='bar', x=df1['Country/Region'], y=df1['Confirmed'], title='Top 10 Countries by Confirmed Cases', xlabel='Country/Region', ylabel='Number of Confirmed Cases', rotation=45, color='orange', edgecolor='black',grid=True)
 
     def plot_death_distribution_pie(self):
-        df2 = self.df.groupby('WHO Region')['Deaths'].sum()
-        visualization_util().plot_chart(kind='pie', x=df2.index, y=df2.values, title='Death Distribution by WHO Region', autopct='%1.1f%%')
+        self.df2 = self.get_sum_from_grouped_df(self.group_dataFrame(self.df, 'WHO Region'), 'Deaths')
+        self.plot_chart(chart_type='pie', x=self.df2.index, y=self.df2.values, title='Death Distribution by WHO Region', autopct='%1.0f%%')
 
     def plot_top5_confirmed_vs_deaths_line(self):
         df3 = self.df[['Country/Region','Confirmed','Deaths']].nlargest(5, 'Confirmed')
-        visualization_util().plot_chart(kind='line', x=df3['Country/Region'], y=df3['Confirmed'], title='Top 5 Countries: Confirmed vs Deaths', xlabel='Country/Region', ylabel='Number of Cases', color='blue', marker='o', label='Confirmed')
-        visualization_util().plot_chart(kind='line', x=df3['Country/Region'], y=df3['Deaths'], color='red', marker='x', label='Deaths')
+        self.plot_chart(chart_type="line", x=df3["Country/Region"], y={"Confirmed": df3["Confirmed"],"Deaths": df3["Deaths"]}, title="Top 5 Countries: Confirmed vs Deaths", xlabel="Country/Region",ylabel="Number of Cases", multi_plot=True)
 
     def plot_confirmed_vs_recovered_scatter(self):
-        visualization_util().plot_chart(kind='scatter', x=self.df['Confirmed'], y=self.df['Recovered'], title='Confirmed vs Recovered Cases', xlabel='Confirmed Cases', ylabel='Recovered Cases', c=self.df['Recovered'], cmap='rainbow_r', alpha=0.6)
+        self.plot_chart(chart_type='scatter', x=self.df['Confirmed'], y=self.df['Recovered'], title='Confirmed vs Recovered Cases', xlabel='Confirmed Cases', ylabel='Recovered Cases', c=self.df['Recovered'], cmap='rainbow_r', alpha=0.6)
 
     def plot_death_counts_hist(self):
-        df5 = self.df.groupby('WHO Region')['Deaths'].sum()
-        visualization_util().plot_chart(kind='hist', x=df5.values, title='Distribution of Death Counts by WHO Region', xlabel='Number of Deaths', ylabel='Frequency', bins=10, color='purple', edgecolor='black')
+        self.plot_chart(chart_type='hist', x=self.df2.values, title='Distribution of Death Counts by WHO Region', xlabel='Number of Deaths', ylabel='Frequency', bins=10, color='purple', edgecolor='black')
 
     def plot_confirmed_deaths_recovered_stacked_bar(self):
         df6 = self.df[['Country/Region','Confirmed','Deaths','Recovered']].head(5)
-        visualization_util().plot_chart(kind='bar', x=df6['Country/Region'], y=None, title='Top 5 Countries: Confirmed, Deaths, and Recovered Cases', xlabel='Country/Region', ylabel='Number of Cases', rotation=45)
-        visualization_util().plot_chart(kind='bar', x=df6['Country/Region'], bottom=df6['Confirmed'], label='Deaths', color='salmon')  # To initialize the bar chart
-        visualization_util().plot_chart(kind='bar', x=df6['Country/Region'], bottom=df6['Confirmed']+df6['Deaths'], label='Recovered', color='lightgreen')  # To initialize the bar chart
+        self.plot_chart(chart_type="stacked_bar", data=df6 ,x="Country/Region",   y=["Confirmed","Deaths","Recovered"], title="Stacked Bar Chart", xlabel='Country/Region', ylabel='Number of Cases', grid=True, label = ["Confirmed","Deaths","Recovered"])
     
     def plot_confirmed_boxplot_by_region(self):
-        visualization_util().plot_chart(kind='box', x = 'WHO Region',  y = 'Confirmed', data =self.df,title='Boxplot of Confirmed Cases by WHO Region', xlabel='WHO Region', ylabel='Number of Confirmed Cases')
+        self.plot_chart(chart_type='box', x = 'WHO Region',  y = 'Confirmed', data =self.df,title='Boxplot of Confirmed Cases by WHO Region', xlabel='WHO Region', ylabel='Number of Confirmed Cases')
 
-    def plot_india_vs_country_trend(self):
-        visualization_util.plot_country_trend(self.df, countries=['India', 'Algeria'], metric='Confirmed')
+    def plot_india_vs_country_trend(self, country):
+        india = self.df[self.df['Country/Region'] == 'India'][['Confirmed','Deaths','Recovered']].iloc[0]
+        other_country = self.df[self.df['Country/Region'] == country][['Confirmed','Deaths','Recovered']].iloc[0]
+        categories = ['Confirmed','Deaths','Recovered']
+        self.plot_chart(chart_type="line", x = categories, y={"India": india.values, country : other_country.values}, title="Covid Cases Comparison: India vs " + country, xlabel="Number of Cases", multi_plot=True)        
 
    
 if __name__ == "__main__":
@@ -49,4 +48,4 @@ if __name__ == "__main__":
     covid_analysis.plot_death_counts_hist()
     covid_analysis.plot_confirmed_deaths_recovered_stacked_bar()
     covid_analysis.plot_confirmed_boxplot_by_region()
-    covid_analysis.plot_india_vs_country_trend()
+    covid_analysis.plot_india_vs_country_trend("Algeria")
--- Week 5/utils/visualization_util.py
+++ Week 5/utils/visualization_util.py
@@ -1,82 +1,76 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
+
 
 
 class visualization_util:
 
-    def plot_chart(self, kind, x, y=None, data = None, title=None, xlabel=None, ylabel=None, rotation=30, grid=True, **user_params):
+    def plot_chart(self, chart_type, x, y=None, data = None, title=None, xlabel=None, ylabel=None, rotation=30, grid=True, multi_plot = None , **user_params):
+         
+        if chart_type == "bar":
+            additional_param = {"width": 0.7, "align": "center", "edgecolor": "black"}
+            additional_param.update(user_params)
+            plt.bar(x, y, **user_params)
 
-            
-        if kind == "bar":
-            params = {"width": 0.7, "align": "center", "edgecolor": "black"}
-            params.update(user_params)
-            plt.bar(x, y, **params)
+        elif chart_type == "line":
+            if multi_plot:
+                for label, y_values in y.items():
+                    additional_param = {"linestyle": "--", "linewidth": 2, "marker": random.choice(["o", "x", "s", "d", "^", "v", "<", ">", "p", "*"])}
+                    additional_param.update(user_params)
+                    plt.plot(x, y_values,label=label, **additional_param)
+            else:
+                additional_param = {"linestyle": "--", "linewidth": 2, "marker": "o"}
+                additional_param.update(user_params)
+                plt.plot(x, y, **additional_param)
 
-        elif kind == "line":
-            params = {"linestyle": "-", "linewidth": 2, "marker": "o"}
-            params.update(user_params)
-            plt.plot(x, y, **params)
-
-        elif kind == "scatter":
-            params = {"s": 100, "marker": "o", "edgecolor": "black", "cmap": "viridis"}
-            params.update(user_params)
-            plt.scatter(x, y, **params)
+        elif chart_type == "scatter":
+            additional_param = {"s": 100, "marker": "o", "edgecolor": "black", "cmap": "viridis"}
+            additional_param.update(user_params)
+            plt.scatter(x, y, **additional_param)
             plt.colorbar()
 
-        elif kind == "hist":
-            params = {"bins": 10, "edgecolor": "black", "histtype": "bar"}
-            params.update(user_params)
-            plt.hist(x, **params)
+        elif chart_type == "hist":
+            additional_param = {"bins": 10, "edgecolor": "black", "histtype": "bar"}
+            additional_param.update(user_params)
+            plt.hist(x, **additional_param)
 
-        elif kind == "pie":
-            params = {"autopct": "%1.1f%%", "startangle": 90}
-            params.update(user_params)
-            plt.pie(y, labels=x, **params)
-
+        elif chart_type == "pie":
+            additional_param = {"autopct": "%1.1f%%", "startangle": 90}
+            additional_param.update(user_params)
+            plt.pie(y, labels=x, **additional_param)
         
-        elif kind == "box":
-            params = {}
-            params.update(user_params)
-            data.boxplot(by = x, column=y, **params)
+        elif chart_type == "box":
+            additional_param = {}
+            additional_param.update(user_params)
+            data.boxplot(by = x, column=y, **additional_param)
 
+        elif chart_type == "stacked_bar":
+            bottom = 0
+            for col in y:
+                plt.bar(data[x], data[col], bottom=bottom, label=col)
+                bottom = bottom + data[col]
 
         else:
-            raise ValueError("Invalid kind. Use: bar, line, scatter, hist, pie")
+            raise ValueError("Invalid chart_type. Use: bar, line, scatter, hist, pie")
         
         if title:
             plt.title(title)
 
-        if xlabel and kind not in ["pie", "hist"]:
+        if xlabel and chart_type not in ["pie", "hist"]:
             plt.xlabel(xlabel)
 
-        if ylabel and kind not in ["pie"]:
+        if ylabel and chart_type not in ["pie"]:
             plt.ylabel(ylabel)
 
-        if rotation and kind not in ["pie"]:
+        if rotation and chart_type not in ["pie"]:
             plt.xticks(rotation=rotation)
         
-        if grid and kind not in ["pie"]:
+        if grid and chart_type not in ["pie"]:
             plt.grid(True, linestyle="--", alpha=0.6)
 
-        if user_params.get("label"):
+        if user_params.get("label") or multi_plot:
             plt.legend()
 
-        plt.tight_layout()
-        plt.show()
-    
-
-    def plot_country_trend(df, countries, metric):
-        plt.figure(figsize=(10,6))
-
-        for country in countries:
-            country_data = df[df['Country/Region'] == country]
-            plt.plot(country_data['Date'], country_data[metric], marker="o", linestyle="--", label=country)
-
-        plt.title(f"{metric} Cases Trend: {' vs '.join(countries)}")
-        plt.xlabel("Date")
-        plt.ylabel(f"Number of {metric} Cases")
-        plt.xticks(rotation=45)
-        plt.grid(True, linestyle="--", alpha=0.6)
-        plt.legend()
         plt.tight_layout()
         plt.show()
